d4109a81cbf06de785f74c11eeab29ff7313d471
cloudmanyh <yanhui13@nudt.edu.cn>
2020-08-10 21:11:20 +0800

    匈牙利封装

--- broker/newtest.py
+++ broker/newtest.py
@@ -1,7 +1,59 @@
 import graph
-import networkx as nx
-import xiongyali.py
+import xiongyali as XYL
+import numpy as np  # 使用import导入模块numpy，并简写成np
+import pandas as pd
+from paraTags import paraTags
+
+# 将混合列表转化为一维列表
+def flatten(input_list):
+    output_list = []
+    while True:
+        if input_list == []:
+            break
+        for index, value in enumerate(input_list):
+            # index :索引序列  value:索引序列对应的值
+            # enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列，
+            # 同时列出数据和数据下标，一般用在 for 循环当中。
+            if type(value) == list:
+                input_list = value + input_list[index+1:]
+                break   # 这里跳出for循环后，从While循环进入的时候index是更新后的input_list新开始算的。
+            else:
+                output_list.append(value)
+                input_list.pop(index)
+                break
+    return output_list
+
+# 读入CSV文件并存为list类型
+def readCSV2List(filePath):
+    corpus = pd.read_csv(filePath)
+    result = corpus.values.tolist()
+    for k in range(len(result)):
+        tempList = result[k]
+        del tempList[0]
+    if len(result[0]) == 1:
+        # 说明二维列表内部列表只有一个变量，本质上是一维列表，压缩为一维列表
+        result = flatten(result)
+    return result
 
 
 if __name__ == "__main__":
-    
+    distance_matrix = np.array(readCSV2List(paraTags.graphLengthPath))
+    print('原始距离矩阵：\n', distance_matrix)
+    delta_state = [2, -3, -1, 3, -1]
+    cost_matrix, out_id_list, in_id_list = XYL.construct_cost_matrix(
+        delta_state, distance_matrix)
+    print('out_id_list: ', out_id_list)
+    print('in_id_list: ', in_id_list)
+    print('优化目标矩阵：\n', cost_matrix)
+    out_id, in_id, cost_index, total_cost = XYL.optimize(cost_matrix)
+    print('输出方id：', out_id)  # 开销矩阵对应的行索引,对应于输出无人机的区域的列表指示器，需要与对应区域的ID映射
+    print('输入方id：', in_id)  # 对应行索引的最优指派的列索引，对应于输入无人机的区域的列表指示器，需要与对应区域的ID映射
+    print('对应cost：', cost_index)  # 提取每个行索引的最优指派列索引所在的元素的值索引，对应于无人机从输出点到输入点的飞行路径，需要与无人机绑定
+    print('总体cost：', total_cost)  # 提取每个行索引的最优指派列索引所在的元素值的综合，表示本次规划无人机共需要飞行的距离
+    system_strategy = []
+    for i in range(len(out_id)):
+        former = out_id_list[out_id[i]]
+        current = in_id_list[in_id[i]]
+        cost = cost_index[i]
+        system_strategy.append([former, current, cost])
+    print(system_strategy)
--- broker/xiongyali.py
+++ broker/xiongyali.py
@@ -1,22 +1,41 @@
 from scipy.optimize import linear_sum_assignment
-import numpy as np  # 使用import导入模块numpy，并简写成np
-cost = np.random.randint(1,9, (6,6))
+import numpy as np  
+
+# 输入：状态相对变化列表， 无向图各个节点之间最短路径矩阵
+# 功能：根据状态变化情况和无向图最短路径矩阵，构造优化问题cost矩阵
+# 输出：优化问题cost矩阵，输出点图id列表，输入点图id列表
 def construct_cost_matrix(delta_state, distance_matrix):
-    cost_matrix = []
-    
-    return cost_matrix
-print(cost)
+    cost_list = []
+    out_id_list = []
+    in_id_list = []
+    for i in range(len(delta_state)):
+        if delta_state[i] < 0:
+            for j in range(abs(delta_state[i])):
+                out_id_list.append(i)
+        if delta_state[i] > 0:
+            for k in range(delta_state[i]):
+                in_id_list.append(i)
+    for out_id in out_id_list:
+        tmp_cost = []
+        for in_id in in_id_list:
+            tmp_cost.append(distance_matrix[out_id][in_id])
+        cost_list.append(tmp_cost)
+    return np.array(cost_list), out_id_list, in_id_list
+
 # 输入：任务分配成本
 # 功能：匈牙利法找到最优分配方式
-# 输出：最优分配方式对应的输出点编号，输入点编号，对应分配成本
+# 输出：最优分配方式对应的输出点矩阵id列表，输入点id列表，对应分配成本列表，总体成本
 def optimize(cost):
-    row_ind, col_ind=linear_sum_assignment(cost)
-    cost_ind = cost[row_ind,col_ind]
-    total_cost = cost[row_ind,col_ind].sum()
-    return row_ind, col_ind, cost_ind, total_cost
+    out_id, in_id = linear_sum_assignment(cost)
+    cost_index = cost[out_id, in_id]
+    total_cost = round(cost[out_id, in_id].sum(), 1)
+    return out_id, in_id, cost_index, total_cost
 
-row_ind, col_ind, cost_ind, total_cost = optimize(cost)
-print(row_ind)#开销矩阵对应的行索引,对应于输出无人机的区域的列表指示器，需要与对应区域的ID映射
-print(col_ind)#对应行索引的最优指派的列索引，对应于输入无人机的区域的列表指示器，需要与对应区域的ID映射
-print(cost_ind)#提取每个行索引的最优指派列索引所在的元素的值索引，对应于无人机从输出点到输入点的飞行路径，需要与无人机绑定
-print(total_cost)#提取每个行索引的最优指派列索引所在的元素值的综合，表示本次规划无人机共需要飞行的距离
+
+if __name__ == "__main__":
+    cost = np.random.randint(1, 9, (4, 4))
+    row_ind, col_ind, cost_ind, total_cost = optimize(cost)
+    print(row_ind)#开销矩阵对应的行索引,对应于输出无人机的区域的列表指示器，需要与对应区域的ID映射
+    print(col_ind)#对应行索引的最优指派的列索引，对应于输入无人机的区域的列表指示器，需要与对应区域的ID映射
+    print(cost_ind)#提取每个行索引的最优指派列索引所在的元素的值索引，对应于无人机从输出点到输入点的飞行路径，需要与无人机绑定
+    print(total_cost)#提取每个行索引的最优指派列索引所在的元素值的综合，表示本次规划无人机共需要飞行的距离
